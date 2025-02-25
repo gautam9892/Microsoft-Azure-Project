420b7d19a4977c9180acfdec48508332e2c7067d
Gautam Kumawat <gautamkumawat141@gmail.com>
2025-02-25 18:56:58 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,61 @@
+<footer class="site-footer section-padding" id="contact">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-5 me-auto col-12">
+                        <?php
+$sql="SELECT * from tblpage where PageType='contactus'";
+$query = $dbh -> prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $row)
+{               ?>
+                        <h5 class="mb-lg-4 mb-3">Timing</h5>
+
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex">
+
+                                <?php  echo ($row->Timing);?>
+                            </li></ul>
+                            <h5 class="mb-lg-4 mb-3">Email</h5>
+                            <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex">
+                                <?php  echo ($row->Email);?></li>
+                                <br>
+                                 <h5 class="mb-lg-4 mb-3">Contact Number</h5>
+                            <li class="list-group-item d-flex">
+                                <?php  echo ($row->MobileNumber);?></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-2 col-md-6 col-12 my-4 my-lg-0">
+                        <h5 class="mb-lg-4 mb-3">Our Clinic</h5>
+
+                     
+
+                        <p><?php  echo ($row->PageDescription);?></p>
+                    </div>
+<?php $cnt=$cnt+1;}} ?>
+                    <div class="col-lg-3 col-md-6 col-12 ms-auto">
+                        <h5 class="mb-lg-4 mb-2">Socials</h5>
+
+                        <ul class="social-icon">
+                            <li><a href="https://www.facebook.com/profile.php?id=61566247006091" class="social-icon-link bi-facebook"></a></li>
+
+                            <li><a href="https://www.instagram.com/balaji_healthcare_hospital18/" class="social-icon-link bi-instagram"></a></li>
+
+                           
+                        </ul>
+
+                        
+                    </div>
+
+                   
+
+                </div>
+            </section>
+        </footer>
